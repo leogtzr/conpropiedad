1dde0aa62c7b6e4e38197ac183c5199cc401071c
leogtzr <leogutierrezramirez@gmail.com>
2020-03-14 14:32:40 -0700

    add base project

---
+++ code/java/con-propiedad/src/main/java/com/propiedad/words/ConPropiedadApplication.java
@@ -0,0 +1,13 @@
+package com.propiedad.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConPropiedadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConPropiedadApplication.class, args);
+	}
+
+}
---
+++ code/java/con-propiedad/src/test/java/com/propiedad/words/ConPropiedadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propiedad.words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConPropiedadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
