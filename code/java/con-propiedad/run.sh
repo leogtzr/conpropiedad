c478a0c3ed674617971ed0b2c8470b2baacd9e60
leogtzr <leogutierrezramirez@gmail.com>
2020-03-21 20:38:03 -0700

    add control scripts

---
+++ code/java/con-propiedad/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o xtrace
+set -o nounset
+set -o pipefail
+
+readonly work_dir="$(dirname "$(readlink --canonicalize-existing "${0}")")"
+readonly target_dir="${work_dir}/target"
+
+get_jar_to_run() {
+    ls -1 "${target_dir}" | grep --extended-regexp 'jar$'
+}
+
+readonly jar_file="${target_dir}/$(get_jar_to_run)"
+readonly opts="-Dspring.profiles.active=development"
+readonly jar_opts=""
+
+java ${opts} -jar "${jar_file}" ${jar_opts}
+
+exit
