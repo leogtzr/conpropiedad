93d86a625b5c7671fe4a7409d61da36dfc0d0c5b
leogtzr <leogutierrezramirez@gmail.com>
2020-07-08 15:29:34 -0600

    expose html page

---
+++ words.js
@@ -0,0 +1,963 @@
+var words = [
+    {
+      "word": "Entradora",
+      "meaning": "Mujer facilona",
+      "tags": [
+        "fácil",
+        "piruja",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Capulina",
+      "meaning": "mujer de mala vida",
+      "tags": [
+        "piruja",
+        "ramera",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Callonca",
+      "meaning": "prostituta",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Concubina",
+      "meaning": "Prostituta",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Cuclillo",
+      "meaning": "Hombre al que le es infiel su mujer",
+      "tags": [
+        "cornudo"
+      ]
+    },
+    {
+      "word": "Gorrina",
+      "meaning": "Mujer deseada y de mala actitud en su trato social.",
+      "tags": [
+        "insinuante",
+        "wila"
+      ]
+    },
+    {
+      "word": "Congalero",
+      "meaning": "prostituta",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Mondaria",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Halconera",
+      "meaning": "mujer provocativa e insinuante.",
+      "tags": [
+        "insinuante",
+        "provocativa",
+        "wila"
+      ]
+    },
+    {
+      "word": "Chirusa o chiruza",
+      "meaning": "mujer de comportamiento vulgar o afectado.",
+      "tags": [
+        "piruja",
+        "volada",
+        "vulgar",
+        "wila"
+      ]
+    },
+    {
+      "word": "Furcia",
+      "meaning": "Prostituta.",
+      "tags": [
+        "mujerzuela",
+        "prosti",
+        "prostituta",
+        "puta",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Garzón",
+      "meaning": "Joven de vida disoluta.",
+      "tags": [
+        "fresca",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Magalla",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Entrona",
+      "meaning": "Mujer accesible, coqueta y fácil de conquistar.",
+      "tags": [
+        "accesible",
+        "coqueta",
+        "facilona",
+        "piruja",
+        "wila"
+      ]
+    },
+    {
+      "word": "Gabasa",
+      "meaning": "Persona que tiene relaciones sexuales a cambio de dinero.",
+      "tags": [
+        "mujerzuela",
+        "prosti",
+        "prostituta",
+        "puta",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Casquivana",
+      "meaning": "coqueta, fresca, frívola",
+      "tags": [
+        "piruja",
+        "ramera",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Faldero",
+      "meaning": "Mujeriego, cobarde que busca resguardo tras la falda de una mujer para protegerse.",
+      "tags": [
+        "cobarde",
+        "mujeriego",
+        "pussy",
+        "puto"
+      ]
+    },
+    {
+      "word": "Cucha",
+      "meaning": "Prostituta, ramera, mujer de mala vida.",
+      "tags": [
+        "piruja",
+        "puta",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pelleja",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Mariol",
+      "meaning": "Maricón.",
+      "tags": [
+        "coconut",
+        "culo",
+        "jotazo",
+        "joto",
+        "marica",
+        "pussy",
+        "puto"
+      ]
+    },
+    {
+      "word": "Rabiza",
+      "meaning": "Ramera muy despreciable.",
+      "tags": [
+        "despreciable",
+        "golfa",
+        "prostituta",
+        "puta",
+        "putilla",
+        "ramera"
+      ]
+    },
+    {
+      "word": "Mujercilla",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Huilón",
+      "meaning": "prostituto.",
+      "tags": [
+        "golfo",
+        "prostituto",
+        "puto"
+      ]
+    },
+    {
+      "word": "Pendón",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "mujerzuela",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Jinetera",
+      "meaning": "Persona que busca a sus clientes entre extranjeros.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "sex",
+        "wila"
+      ]
+    },
+    {
+      "word": "Hurgamandero",
+      "meaning": "persona que se vende sexualmente.",
+      "tags": [
+        "golfa",
+        "golfo",
+        "prosti",
+        "prostituta",
+        "prostituto",
+        "puta",
+        "puto",
+        "ramera",
+        "sex"
+      ]
+    },
+    {
+      "word": "Pindanga",
+      "meaning": "Mujer callejera.",
+      "tags": [
+        "callejera",
+        "golfa",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Mesalina",
+      "meaning": "Ramera, prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Lúbrico",
+      "meaning": "Libidinoso, lascivo.",
+      "tags": [
+        "lascivo",
+        "volado"
+      ]
+    },
+    {
+      "word": "Hetera",
+      "meaning": "mujer facil, de cascos ligeros.",
+      "tags": [
+        "fácil",
+        "golfa",
+        "puta",
+        "putilla",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pelota",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pendanga",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Perico",
+      "meaning": "Persona, especialmente mujer, que gusta de callejear y es a veces de vida desenvuelta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Perendeca",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Lumia",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Quelite",
+      "meaning": "Amante.",
+      "tags": [
+        "amante",
+        "canco"
+      ]
+    },
+    {
+      "word": "Mozcorra",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Quillotra",
+      "meaning": "Amiga, amante.",
+      "tags": [
+        "amante",
+        "canco",
+        "querida"
+      ]
+    },
+    {
+      "word": "Pitoflojo",
+      "meaning": "Que se acuesta con cualquiera.",
+      "tags": [
+        "mujeriego",
+        "pitoflojo",
+        "puto",
+        "wilo"
+      ]
+    },
+    {
+      "word": "Putón",
+      "meaning": "Mujer promiscua.",
+      "tags": [
+        "promiscua",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Querida",
+      "meaning": "Amante.",
+      "tags": [
+        "amante",
+        "canco"
+      ]
+    },
+    {
+      "word": "Suripanta",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prostituta",
+        "puta",
+        "putilla",
+        "ramera"
+      ]
+    },
+    {
+      "word": "Madama",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pípila",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "mujerzuela",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Rabisalsera",
+      "meaning": "Se aplica a la mujer viva, desenvuelta y descarada.",
+      "tags": [
+        "desenvuelta",
+        "fresca",
+        "viva",
+        "volada",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pizcapocha",
+      "meaning": "Piruja, ramera.",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Taita",
+      "meaning": "Rufián, hombre que explota prostitutas.",
+      "tags": [
+        "chulo",
+        "pimp",
+        "rufián"
+      ]
+    },
+    {
+      "word": "Quebrachón",
+      "meaning": "Dicho de un homosexual.",
+      "tags": [
+        "jotito",
+        "joto",
+        "marica",
+        "maricanena",
+        "muerdealmohadas",
+        "puto"
+      ]
+    },
+    {
+      "word": "Tronera",
+      "meaning": "Persona de vida desordenada, libertina o disipada.",
+      "tags": [
+        "desordenado",
+        "libertino",
+        "sátiro"
+      ]
+    },
+    {
+      "word": "Pipusa",
+      "meaning": "Mujerzuela.",
+      "tags": [
+        "golfa",
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "putilla",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Pupila",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Verriondo",
+      "meaning": "Lujurioso, libidinoso.",
+      "tags": [
+        "lascivo",
+        "libidinoso",
+        "lujurioso",
+        "sátiro"
+      ]
+    },
+    {
+      "word": "Meretriz",
+      "meaning": "Prostituta.",
+      "tags": [
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Zarrapastro",
+      "meaning": "Mujer de mal vivir.",
+      "tags": [
+        "fresca",
+        "viva",
+        "wila"
+      ]
+    },
+    {
+      "word": "Putarrón",
+      "meaning": "Prostituta de muy baja condición.",
+      "tags": [
+        "barata",
+        "golfa",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "wila"
+      ]
+    },
+    {
+      "word": "Zurrona",
+      "meaning": "Mujer perdida.",
+      "tags": [
+        "perdida",
+        "volada"
+      ]
+    },
+    {
+      "word": "Baldonada",
+      "meaning": "prostituta.",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Cachirila",
+      "meaning": "ramera.",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Coja",
+      "meaning": "prostituta.",
+      "tags": [
+        "mujerzuela",
+        "piruja",
+        "prosti",
+        "prostituta",
+        "puta",
+        "ramera",
+        "sex",
+        "volada",
+        "wila",
+        "zorra"
+      ]
+    },
+    {
+      "word": "Encolado",
+      "meaning": "Hombre presumido y afeminado.",
+      "tags": [
+        "gay",
+        "joto",
+        "marica",
+        "maricón"
+      ]
+    },
+    {
+      "word": "Hadrubado",
+      "meaning": "jorobado.",
+      "tags": [
+        "jorobado",
+        "pipila"
+      ]
+    },
+    {
+      "word": "Amondogado",
+      "meaning": "Gordo.",
+      "tags": [
+        "cochino",
+        "fat",
+        "gordo",
+        "marrano"
+      ]
+    },
+    {
+      "word": "Aporreado",
+      "meaning": "Dicho de una persona físicamente agotada.",
+      "tags": [
+        "agotado",
+        "jodido"
+      ]
+    },
+    {
+      "word": "Adán",
+      "meaning": "Hombre sucio, haraposo, desaliñado.",
+      "tags": [
+        "astroso",
+        "chamagoso",
+        "cochino",
+        "desaliñado",
+        "harapiento",
+        "haraposo",
+        "marrano",
+        "mugriento",
+        "sucio"
+      ]
+    },
+    {
+      "word": "Apersonado",
+      "meaning": "De mala presencia.",
+      "tags": [
+        "desaliñado",
+        "presencia"
+      ]
+    },
+    {
+      "word": "Ascoroso",
+      "meaning": "asqueroso.",
+      "tags": [
+        "asqueroso"
+      ]
+    },
+    {
+      "word": "Bichicome",
+      "meaning": "persona indigente, que vive de desperdicios.",
+      "tags": [
+        "errante",
+        "indigente",
+        "jodido",
+        "vagabundo"
+      ]
+    },
+    {
+      "word": "Birriosa",
+      "meaning": "de mala calidad o aspecto.",
+      "tags": [
+        "apariencia",
+        "desaliñada",
+        "desaliñado"
+      ]
+    },
+    {
+      "word": "Bisunto",
+      "meaning": "sucio, sobado, grasiento.",
+      "tags": [
+        "astroso",
+        "chamagoso",
+        "grasiento",
+        "pergeñado",
+        "sobado",
+        "sucio"
+      ]
+    },
+    {
+      "word": "Bodrio",
+      "meaning": "dicho de una persona físicamente desagradable, descuidada, desarreglada o de mal gusto.",
+      "tags": [
+        "desaliñada",
+        "sobado",
+        "sucio"
+      ]
+    },
+    {
+      "word": "Cachiporra",
+      "meaning": "farsante, vanidoso.",
+      "tags": [
+        "farsante",
+        "marica",
+        "vanidoso"
+      ]
+    },
+    {
+      "word": "Caima",
+      "meaning": "dicho de una persona o cosa sin gracia.",
+      "tags": [
+        "aguado",
+        "gracia",
+        "payaso"
+      ]
+    },
+    {
+      "word": "Callo",
+      "meaning": "mujer muy fea.",
+      "tags": [
+        "culera",
+        "espantosa",
+        "fea",
+        "gacha",
+        "horrible"
+      ]
+    },
+    {
+      "word": "Carantoña",
+      "meaning": "mujer vieja y fea que se aplica aceites y se compone el rostro para disimular su fealdad.",
+      "tags": [
+        "culera",
+        "fea",
+        "jodida",
+        "vieja"
+      ]
+    },
+    {
+      "word": "Cascado",
+      "meaning": "persona que está muy jodida o traqueteada.",
+      "tags": [
+        "jodida",
+        "jodido",
+        "traqueteado"
+      ]
+    },
+    {
+      "word": "Catzagua",
+      "meaning": "mugriento, chamagoso, sucio.",
+      "tags": [
+        "chamagoso",
+        "mugriento",
+        "sucio"
+      ]
+    },
+    {
+      "word": "Cermeño",
+      "meaning": "hombre tosco, sucio, necio.",
+      "tags": [
+        "chamagoso",
+        "hombre",
+        "necio",
+        "sucio",
+        "tosco"
+      ]
+    },
+    {
+      "word": "Choclón",
+      "meaning": "desaliñado, mal vestido.",
+      "tags": [
+        "aspecto",
+        "desaliñado",
+        "vestido"
+      ]
+    },
+    {
+      "word": "Chungo",
+      "meaning": "de mal aspecto, en mal estado o de mala calidad.",
+      "tags": [
+        "desaliñada",
+        "desaliñado",
+        "mal aspecto",
+        "vestido"
+      ]
+    },
+    {
+      "word": "Cuija",
+      "meaning": "Mujer flaca y fea.",
+      "tags": [
+        "culera",
+        "escuálida",
+        "fea",
+        "flaca",
+        "jodida"
+      ]
+    },
+    {
+      "word": "Desandrajado",
+      "meaning": "Andrajoso, desastrado.",
+      "tags": [
+        "andrajosa",
+        "andrajoso",
+        "desastrado",
+        "jodido",
+        "vagabundo"
+      ]
+    },
+    {
+      "word": "Desapuesto",
+      "meaning": "Desarreglado, de mala disposición y presencia.",
+      "tags": [
+        "desaliñada",
+        "desaliñado",
+        "desarreglado",
+        "presencia"
+      ]
+    },
+    {
+      "word": "Desaseado",
+      "meaning": "Falto de aseo.",
+      "tags": [
+        "aseo",
+        "cochino",
+        "desaseado",
+        "marrano",
+        "mugroso"
+      ]
+    },
+    {
+      "word": "Descachalandrado",
+      "meaning": "Desaliñado, andrajoso.",
+      "tags": [
+        "andrajoso",
+        "desaliñada",
+        "desaliñado",
+        "descuidado"
+      ]
+    },
+    {
+      "word": "Descarrachado",
+      "meaning": "persona descuidada o desaliñada.",
+      "tags": [
+        "desaliñado",
+        "desarreglado",
+        "descuidado"
+      ]
+    },
+    {
+      "word": "Desemejado",
+      "meaning": "Terrible, desfigurado, muy feo.",
+      "tags": [
+        "culero",
+        "deforme",
+        "fea",
+        "feo"
+      ]
+    },
+    {
+      "word": "Desguañado",
+      "meaning": "Desarreglado.",
+      "tags": [
+        "desaliñada",
+        "desaliñado",
+        "desarreglado",
+        "presencia"
+      ]
+    },
+    {
+      "word": "Disforme",
+      "meaning": "Feo, horroroso, monstruoso.",
+      "tags": [
+        "culera",
+        "culero",
+        "fea",
+        "feo",
+        "horroroso",
+        "monstruoso"
+      ]
+    }
+  ];
